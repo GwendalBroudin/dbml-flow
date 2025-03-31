fbc0709ec1a4f0efaed9a948a93ff867147e5cfc
Gwendal Broudin <gwendalbroudin@gmail.com>
2025-03-31 11:48:32 +0000

    set back router

--- src/routes/view.tsx
+++ src/routes/view.tsx
@@ -1,8 +1,11 @@
 import Viewer from "@/components/viewer/viewer";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, retainSearchParams } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/view")({
   component: RouteComponent,
+  search: {
+    middlewares: [retainSearchParams(true)],
+  },
 });
 
 function RouteComponent() {
