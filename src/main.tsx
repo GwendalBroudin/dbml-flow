4956bc5b26ce6628e2bdfcbc73d8691fc5d7dea7
Gwendal Broudin <gwendalbroudin@gmail.com>
2025-04-18 09:22:06 +0000

    remove tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,9 @@
-import "@/index.css";
-import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-// Import the generated route tree
-import { routeTree } from "./routeTree.gen";
-import { basePath } from "./constant";
+import "./index.css";
 
-// Create a new router instance
-const router = createRouter({ routeTree, basepath: basePath });
+const container = document.querySelector("#app");
+const root = createRoot(container!);
 
-// Register the router instance for type safety
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
-}
-
-// Render the app
-const rootElement = document.getElementById("app")!;
-if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>
-  );
-}
+root.render(<App />);
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
